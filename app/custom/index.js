fc162c22e3b7455a71236213c7d225e03c726ef4
YEO <dugudcjfwin@lgcns.com>
2021-02-26 12:24:09 +0900

    edit context pad

--- app/custom/index.js
+++ app/custom/index.js
@@ -3,8 +3,9 @@ import CustomPalette from './CustomPalette';
 import CustomRenderer from './CustomRenderer';
 
 export default {
-  __init__: [ 'customContextPad', 'customPalette', 'customRenderer' ],
-  customContextPad: [ 'type', CustomContextPad ],
+  //customContextPad
+  __init__: [ 'customPalette', 'customRenderer' ],
+  //customContextPad: [ 'type', CustomContextPad ],
   customPalette: [ 'type', CustomPalette ],
   customRenderer: [ 'type', CustomRenderer ]
 
