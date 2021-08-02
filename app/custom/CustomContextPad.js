de5436911e55cb4be81954f877e56c42fab84e07
71575 <gschun@lgcns.com>
2021-08-02 13:49:35 +0900

    colorPick 오류수정

--- app/custom/CustomContextPad.js
+++ app/custom/CustomContextPad.js
@@ -203,184 +203,184 @@ function openColorPicker (element)
 
 
   //color picker..
-  $.fn.colorPick = function (config) {
+  // $.fn.colorPick = function (config) {
 
-    return this.each(function () {
-      new $.colorPick(this, config || {});
-    });
+  //   return this.each(function () {
+  //     new $.colorPick(this, config || {});
+  //   });
 
-  };
+  // };
 
-  $.colorPick = function (element, options) {
-    options = options || {};
-    this.options = $.extend({}, $.fn.colorPick.defaults, options);
-    if (options.str) {
-      this.options.str = $.extend({}, $.fn.colorPick.defaults.str, options.str);
-    }
-    $.fn.colorPick.defaults = this.options;
-    this.color = this.options.initialColor.toUpperCase();
-    this.element = $(element);
+  // $.colorPick = function (element, options) {
+  //   options = options || {};
+  //   this.options = $.extend({}, $.fn.colorPick.defaults, options);
+  //   if (options.str) {
+  //     this.options.str = $.extend({}, $.fn.colorPick.defaults.str, options.str);
+  //   }
+  //   $.fn.colorPick.defaults = this.options;
+  //   this.color = this.options.initialColor.toUpperCase();
+  //   this.element = $(element);
 
-    var dataInitialColor = this.element.data('initialcolor');
-    if (dataInitialColor) {
-      this.color = dataInitialColor;
-      this.appendToStorage(this.color);
-    }
+  //   var dataInitialColor = this.element.data('initialcolor');
+  //   if (dataInitialColor) {
+  //     this.color = dataInitialColor;
+  //     this.appendToStorage(this.color);
+  //   }
 
-    var uniquePalette = [];
-    $.each($.fn.colorPick.defaults.palette.map(function (x) { return x.toUpperCase() }), function (i, el) {
-      if ($.inArray(el, uniquePalette) === -1) uniquePalette.push(el);
-    });
+  //   var uniquePalette = [];
+  //   $.each($.fn.colorPick.defaults.palette.map(function (x) { return x.toUpperCase() }), function (i, el) {
+  //     if ($.inArray(el, uniquePalette) === -1) uniquePalette.push(el);
+  //   });
 
-    this.palette = uniquePalette;
+  //   this.palette = uniquePalette;
 
-    return this.element.hasClass(this.options.pickrclass) ? this : this.init();
-  };
+  //   return this.element.hasClass(this.options.pickrclass) ? this : this.init();
+  // };
 
-  $.fn.colorPick.defaults = {
-    'initialColor': '#3498db',
-    'paletteLabel': 'Default palette:',
-    'allowRecent': true,
-    'recentMax': 5,
-    'allowCustomColor': false,
-    'palette': ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", 
-                "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", 
-                "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", 
-                "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"],
-    'onColorSelected': function () {
-      this.element.css({ 'backgroundColor': this.color, 'color': this.color });
-    }
-  };
+  // $.fn.colorPick.defaults = {
+  //   'initialColor': '#3498db',
+  //   'paletteLabel': 'Default palette:',
+  //   'allowRecent': true,
+  //   'recentMax': 5,
+  //   'allowCustomColor': false,
+  //   'palette': ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", 
+  //               "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", 
+  //               "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", 
+  //               "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"],
+  //   'onColorSelected': function () {
+  //     this.element.css({ 'backgroundColor': this.color, 'color': this.color });
+  //   }
+  // };
 
-  $.colorPick.prototype = {
+  // $.colorPick.prototype = {
 
-    init: function () {
+  //   init: function () {
 
-      var self = this;
-      var o = this.options;
+  //     var self = this;
+  //     var o = this.options;
 
-      $.proxy($.fn.colorPick.defaults.onColorSelected, this)();
+  //     $.proxy($.fn.colorPick.defaults.onColorSelected, this)();
 
-      this.element.click(function (event) {
-        var offset = $(self.element).offset();
+  //     this.element.click(function (event) {
+  //       var offset = $(self.element).offset();
 
-        event.preventDefault();
-        self.show(self.element, event.pageX - offset.left, event.pageY - offset.top);
+  //       event.preventDefault();
+  //       self.show(self.element, event.pageX - offset.left, event.pageY - offset.top);
 
-        $('.customColorHash').val(self.color);
+  //       $('.customColorHash').val(self.color);
 
-        $('.colorPickButton').click(function (event) {
-          self.color = $(event.target).attr('hexValue');
-          self.appendToStorage($(event.target).attr('hexValue'));
-          self.hide();
-          $.proxy(self.options.onColorSelected, self)();
-          return false;
-        });
-        $('.customColorHash').click(function (event) {
-          return false;
-        }).keyup(function (event) {
-          var hash = $(this).val();
-          if (hash.indexOf('#') !== 0) {
-            hash = "#" + hash;
-          }
-          if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(hash)) {
-            self.color = hash;
-            self.appendToStorage(hash);
-            $.proxy(self.options.onColorSelected, self)();
-            $(this).removeClass('error');
-          } else {
-            $(this).addClass('error');
-          }
-        });
+  //       $('.colorPickButton').click(function (event) {
+  //         self.color = $(event.target).attr('hexValue');
+  //         self.appendToStorage($(event.target).attr('hexValue'));
+  //         self.hide();
+  //         $.proxy(self.options.onColorSelected, self)();
+  //         return false;
+  //       });
+  //       $('.customColorHash').click(function (event) {
+  //         return false;
+  //       }).keyup(function (event) {
+  //         var hash = $(this).val();
+  //         if (hash.indexOf('#') !== 0) {
+  //           hash = "#" + hash;
+  //         }
+  //         if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(hash)) {
+  //           self.color = hash;
+  //           self.appendToStorage(hash);
+  //           $.proxy(self.options.onColorSelected, self)();
+  //           $(this).removeClass('error');
+  //         } else {
+  //           $(this).addClass('error');
+  //         }
+  //       });
 
-        return false;
-      }).blur(function () {
-        self.element.val(self.color);
-        $.proxy(self.options.onColorSelected, self)();
-        self.hide();
-        return false;
-      });
+  //       return false;
+  //     }).blur(function () {
+  //       self.element.val(self.color);
+  //       $.proxy(self.options.onColorSelected, self)();
+  //       self.hide();
+  //       return false;
+  //     });
 
-      $(document).on('click', function (event) {
-        self.hide();
-        return true;
-      });
+  //     $(document).on('click', function (event) {
+  //       self.hide();
+  //       return true;
+  //     });
 
-      return this;
-    },
+  //     return this;
+  //   },
 
-    appendToStorage: function (color) {
-      if ($.fn.colorPick.defaults.allowRecent === true) {
-        var storedColors = JSON.parse(localStorage.getItem("colorPickRecentItems"));
-        if (storedColors == null) {
-          storedColors = [];
-        }
-        if ($.inArray(color, storedColors) == -1) {
-          storedColors.unshift(color);
-          storedColors = storedColors.slice(0, $.fn.colorPick.defaults.recentMax)
-          localStorage.setItem("colorPickRecentItems", JSON.stringify(storedColors));
-        }
-      }
-    },
+  //   appendToStorage: function (color) {
+  //     if ($.fn.colorPick.defaults.allowRecent === true) {
+  //       var storedColors = JSON.parse(localStorage.getItem("colorPickRecentItems"));
+  //       if (storedColors == null) {
+  //         storedColors = [];
+  //       }
+  //       if ($.inArray(color, storedColors) == -1) {
+  //         storedColors.unshift(color);
+  //         storedColors = storedColors.slice(0, $.fn.colorPick.defaults.recentMax)
+  //         localStorage.setItem("colorPickRecentItems", JSON.stringify(storedColors));
+  //       }
+  //     }
+  //   },
 
-    show: function (element, left, top) {
+  //   show: function (element, left, top) {
 
-      $(".colorPickWrapper").remove();
+  //     $(".colorPickWrapper").remove();
 
-      $(element).prepend('<div class="colorPickWrapper"><div id="colorPick" style="display:none;top:' + top + 'px;left:' + left + 'px"><span>' + $.fn.colorPick.defaults.paletteLabel + '</span></div></div>');
+  //     $(element).prepend('<div class="colorPickWrapper"><div id="colorPick" style="display:none;top:' + top + 'px;left:' + left + 'px"><span>' + $.fn.colorPick.defaults.paletteLabel + '</span></div></div>');
 
-      jQuery.each(this.palette, function (index, item) {
-        $("#colorPick").append('<div class="colorPickButton" hexValue="' + item + '" style="background:' + item + '"></div>');
-      });
-      if ($.fn.colorPick.defaults.allowCustomColor === true) {
-        $("#colorPick").append('<input type="text" style="margin-top:5px" class="customColorHash" />');
-      }
-      if ($.fn.colorPick.defaults.allowRecent === true) {
-        $("#colorPick").append('<span style="margin-top:5px">Recent:</span>');
-        if (JSON.parse(localStorage.getItem("colorPickRecentItems")) == null || JSON.parse(localStorage.getItem("colorPickRecentItems")) == []) {
-          $("#colorPick").append('<div class="colorPickButton colorPickDummy"></div>');
-        } else {
-          jQuery.each(JSON.parse(localStorage.getItem("colorPickRecentItems")), function (index, item) {
-            $("#colorPick").append('<div class="colorPickButton" hexValue="' + item + '" style="background:' + item + '"></div>');
-            if (index == $.fn.colorPick.defaults.recentMax - 1) {
-              return false;
-            }
-          });
-        }
-      }
-      $("#colorPick").fadeIn(200);
-    },
+  //     jQuery.each(this.palette, function (index, item) {
+  //       $("#colorPick").append('<div class="colorPickButton" hexValue="' + item + '" style="background:' + item + '"></div>');
+  //     });
+  //     if ($.fn.colorPick.defaults.allowCustomColor === true) {
+  //       $("#colorPick").append('<input type="text" style="margin-top:5px" class="customColorHash" />');
+  //     }
+  //     if ($.fn.colorPick.defaults.allowRecent === true) {
+  //       $("#colorPick").append('<span style="margin-top:5px">Recent:</span>');
+  //       if (JSON.parse(localStorage.getItem("colorPickRecentItems")) == null || JSON.parse(localStorage.getItem("colorPickRecentItems")) == []) {
+  //         $("#colorPick").append('<div class="colorPickButton colorPickDummy"></div>');
+  //       } else {
+  //         jQuery.each(JSON.parse(localStorage.getItem("colorPickRecentItems")), function (index, item) {
+  //           $("#colorPick").append('<div class="colorPickButton" hexValue="' + item + '" style="background:' + item + '"></div>');
+  //           if (index == $.fn.colorPick.defaults.recentMax - 1) {
+  //             return false;
+  //           }
+  //         });
+  //       }
+  //     }
+  //     $("#colorPick").fadeIn(200);
+  //   },
 
-    hide: function () {
-      $(".colorPickWrapper").fadeOut(200, function () {
-        $(".colorPickWrapper").remove();
-        return this;
-      });
-    },
+  //   hide: function () {
+  //     $(".colorPickWrapper").fadeOut(200, function () {
+  //       $(".colorPickWrapper").remove();
+  //       return this;
+  //     });
+  //   },
 
-  };
+  // };
 
-  $("#picker1").colorPick({
-    'initialColor': 'hsl(0,0%,97%)',
-    'palette': ["#1abc9c", "#16a085", "#2ecc71",
-                "#2980b9", "#9b59b6", "#8e44ad",
-                "#f1c40f", "#f39c12", "#e67e22",
-                "#c0392b", "#ecf0f1", "#00ff0000"],
-    'onColorSelected': function () {
-      console.log("The user has selected the color: " + this.color)
-      this.element.css({ 'backgroundColor': this.color, 'color': this.color });
+  // $("#picker1").colorPick({
+  //   'initialColor': 'hsl(0,0%,97%)',
+  //   'palette': ["#1abc9c", "#16a085", "#2ecc71",
+  //               "#2980b9", "#9b59b6", "#8e44ad",
+  //               "#f1c40f", "#f39c12", "#e67e22",
+  //               "#c0392b", "#ecf0f1", "#00ff0000"],
+  //   'onColorSelected': function () {
+  //     console.log("The user has selected the color: " + this.color)
+  //     this.element.css({ 'backgroundColor': this.color, 'color': this.color });
 
-      // var modeling = bpmnModeler.get('modeling');
+  //     var modeling = bpmnModeler.get('modeling');
       
-      // for (var i = 0; i < bpmnModeler.get('selection').get().length; i++) {
+  //     for (var i = 0; i < bpmnModeler.get('selection').get().length; i++) {
 
-      //  modeling.setColor(bpmnModeler.get('selection').get()[i], {
-      //    stroke: 'black',
-      //    fill: this.color
-      //  });
-      //   elementsToColor.push(bpmnModeler.get('selection').get()[i]);
-      }
-    });
+  //      modeling.setColor(bpmnModeler.get('selection').get()[i], {
+  //        stroke: 'black',
+  //        fill: this.color
+  //      });
+  //       elementsToColor.push(bpmnModeler.get('selection').get()[i]);
+  //     }
+  //   });
   
 
 
